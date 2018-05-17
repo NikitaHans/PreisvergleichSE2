bddda47221d352ea3fbca519a4d5c32f5f32711f
NikitaHans <nikita.hans@hm.edu>
2018-05-17 23:49:33 +0200

    Enableing chaning of addType method [@Brand] Refactoring Mock Replacing inclAttributes array with attributes ArrayList [@CarPackage]

--- src/main/java/edu/hm/shareit/Services/CarServiceMock.java
+++ src/main/java/edu/hm/shareit/Services/CarServiceMock.java
@@ -2,152 +2,100 @@ package edu.hm.shareit.Services;
 
 import edu.hm.shareit.models.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CarServiceMock implements CarServiceFunctionality {
 
-    Brand b1;
-    Brand b2;
-    Brand b3;
-    Brand b4;
-    Brand b5;
+    private ArrayList<Brand> brandList;
+    private ArrayList<CarAttribute> attributeList;
+    private ArrayList<CarPackage> packageList;
 
-    Brand[] brandList;
-
-    BrandType bt1;
-    BrandType bt2;
-    BrandType bt3;
-    BrandType bt4;
-    BrandType bt5;
-    BrandType bt6;
-    BrandType bt7;
-    BrandType bt8;
-    BrandType bt9;
-    BrandType bt10;
-
-    CarAttribute c1;
-    CarAttribute c2;
-    CarAttribute c3;
-    CarAttribute c4;
-    CarAttribute c5;
-    CarAttribute c6;
-
-    CarPackage cp1;
-    CarPackage cp2;
-    CarPackage cp3;
-
-    BrandType[] brandTypeList;
-
-    CarAttribute[] attributeList;
-
-    CarPackage[] packageList;
+    private final String[] defaultBrands = {"BMW", "Audi", "Merceds", "VW", "Skoda"};
+    private final String[] defaultBrandTypes = {"M1", "M3", "A3", "A8", "S", "C", "Golf", "Tiguan", "Yeti", "Octavia"};
 
     public CarServiceMock(){
-        brandList = new Brand[5];
-        brandTypeList = new BrandType[10];
-        attributeList = new CarAttribute[6];
-        packageList = new CarPackage[3];
+        setUpDefaultBrands();
+        setUpDefaultCarPackages();
+    }
 
-        b1 = new Brand("BMW");
-        b2 = new Brand("Audi");
-        b3 = new Brand("Mercedes");
-        b4 = new Brand("VW");
-        b5 = new Brand("Skoda");
+    private void setUpDefaultBrands() {
+        int index = 0;
+        brandList = new ArrayList<>();
+        for( String brandName : defaultBrands) {
+            brandList.add(
+                    new Brand(brandName)
+                            .addType(new BrandType(defaultBrandTypes[index]))
+                            .addType(new BrandType(defaultBrandTypes[index + 1]))
+            );
+            index++;
+        }
+    }
 
-        bt1 = new BrandType("M1");
-        bt2 = new BrandType("M3");
-        bt3 = new BrandType("A3");
-        bt4 = new BrandType("A8");
-        bt5 = new BrandType("S");
-        bt6 = new BrandType("C");
-        bt7 = new BrandType("Golf");
-        bt8 = new BrandType("Tiguan");
-        bt9 = new BrandType("Yeti");
-        bt10 = new BrandType("Octavia");
+    private void setUpDefaultCarPackages() {
+        packageList = new ArrayList<>();
 
-        c1 = new CarAttribute("Climacontrol", ClimateZone.HOT);
-        c2 = new CarAttribute("Heating", ClimateZone.COLD);
-        c3 = new CarAttribute("Navigation", ClimateZone.NORMAL);
-        c4 = new CarAttribute("Audio", ClimateZone.NORMAL);
-        c5 = new CarAttribute("Window winder", ClimateZone.NORMAL);
-        c6 = new CarAttribute("Snow chains", ClimateZone.COLD);
+        attributeList = new ArrayList<>();
+        attributeList.add(new CarAttribute("Climacontrol", ClimateZone.HOT ));
+        attributeList.add(new CarAttribute("Heating", ClimateZone.COLD));
+        attributeList.add(new CarAttribute("Navigation", ClimateZone.NORMAL));
+        attributeList.add(new CarAttribute("Audio", ClimateZone.NORMAL));
+        attributeList.add(new CarAttribute("Window winder", ClimateZone.NORMAL));
+        attributeList.add(new CarAttribute("Snow chains", ClimateZone.COLD));
 
-        cp1 = new CarPackage("Sport", new CarAttribute[]{c1, c4, c6});
-        cp2 = new CarPackage("Normal", new CarAttribute[]{c1, c2, c5});
-        cp3 = new CarPackage("Premium", new CarAttribute[]{c1, c2, c3, c4, c5, c6});
+        HashMap<String, int[]>defaultCarPackages = new HashMap<>();
+        defaultCarPackages.put("Sport",new int[]{0,3,5});
+        defaultCarPackages.put("Normal",new int[]{0,1,4});
+        defaultCarPackages.put("Premium",new int[]{0,1,2,3,4,5});
 
-        b1.addType(bt1);
-        b1.addType(bt2);
-        b2.addType(bt3);
-        b2.addType(bt4);
-        b3.addType(bt5);
-        b3.addType(bt6);
-        b4.addType(bt7);
-        b4.addType(bt8);
-        b5.addType(bt9);
-        b5.addType(bt10);
-
-        brandList[0] = b1;
-        brandList[1] = b2;
-        brandList[2] = b3;
-        brandList[3] = b4;
-        brandList[4] = b5;
-
-        brandTypeList[0] = bt1;
-        brandTypeList[1] = bt2;
-        brandTypeList[2] = bt3;
-        brandTypeList[3] = bt4;
-        brandTypeList[4] = bt5;
-        brandTypeList[5] = bt6;
-        brandTypeList[6] = bt7;
-        brandTypeList[7] = bt8;
-        brandTypeList[8] = bt9;
-        brandTypeList[9] = bt10;
-
-        attributeList[0] = c1;
-        attributeList[1] = c2;
-        attributeList[2] = c3;
-        attributeList[3] = c4;
-        attributeList[4] = c5;
-        attributeList[5] = c6;
-
-        packageList[0] = cp1;
-        packageList[1] = cp2;
-        packageList[2] = cp3;
+        for (Map.Entry<String, int[]> entry : defaultCarPackages.entrySet()) {
+            CarPackage carPackage = new CarPackage(entry.getKey());
+            for( int index : entry.getValue()) {
+                carPackage.addAttributes(attributeList.get(index));
+            }
+            packageList.add(carPackage);
+        }
     }
 
     @Override
     public Brand[] getBrands() {
-        return brandList;
+        Brand[] array = new Brand[brandList.size()];
+        return brandList.toArray(array);
     }
 
     @Override
     public BrandType[] getTypes(Brand brand) {
-       BrandType[] res;
-
-       for (int i = 0; i < brandList.length; i++){
-           if(brandList[i].getName().equals(brand.getName())){
-               List<BrandType> tmp = brandList[i].getTypes();
-               res = tmp.toArray(new BrandType[tmp.size()]);
-               return res;
+       for (Brand brandA : brandList){
+           if(brandA.getName().equals(brand.getName())){
+               return (BrandType[]) brandA.getTypes().toArray();
            }
        }
-       return res = new BrandType[0];
+       return new BrandType[0];
     }
 
     @Override
     public BrandType[] getAllTypes() {
-        return brandTypeList;
+        BrandType[] returnTypeArray = new BrandType[defaultBrandTypes.length];
+        int index = 0;
+        for(String brandTypeName : defaultBrandTypes) {
+            returnTypeArray[index] = new BrandType(brandTypeName);
+            index++;
+        }
+        return returnTypeArray;
     }
 
     @Override
     public CarPackage[] getPakets() {
-        return packageList;
+        CarPackage[] array = new CarPackage[packageList.size()];
+        return packageList.toArray(array);
     }
 
     @Override
     public CarAttribute[] getAttributes() {
-        return attributeList;
+        CarAttribute[] array = new CarAttribute[attributeList.size()];
+        return attributeList.toArray(array);
     }
 
     @Override
--- src/main/java/edu/hm/shareit/models/CarPackage.java
+++ src/main/java/edu/hm/shareit/models/CarPackage.java
@@ -1,20 +1,41 @@
 package edu.hm.shareit.models;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class CarPackage {
 
     private String name;
-    private CarAttribute[] inclAttributes;
+    private ArrayList<CarAttribute> attributes;
+
+    public CarPackage(String name){
+        this.name = name;
+        attributes = new ArrayList<>();
+    }
 
     public CarPackage(String name, CarAttribute[] attributes){
-        this.name = name;
-        this.inclAttributes = attributes;
+        this(name);
+        Collections.addAll(this.attributes, attributes);
     }
 
     public String getName() {
         return name;
     }
 
-    public CarAttribute[] getInclAttributes() {
-        return inclAttributes;
+    public CarPackage addAttributes(CarAttribute... attributes) {
+        Collections.addAll(this.attributes, attributes);
+        return this;
+    }
+
+    public CarPackage removeAttributes(CarAttribute... attributes) {
+        for( CarAttribute attribute : attributes) {
+            this.attributes.remove(attribute);
+        }
+        return this;
+    }
+
+    public CarAttribute[] getAttributes() {
+        CarAttribute[] array = new CarAttribute[attributes.size()];
+        return attributes.toArray(array);
     }
 }
